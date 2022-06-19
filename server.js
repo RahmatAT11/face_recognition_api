e4424cfcea4d15bfb202697d8839eae2caf7cbb6
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2022-06-19 14:32:48 +0700

    implement database to backend

--- server.js
+++ server.js
@@ -1,95 +1,103 @@
-import express from "express";
+import express, { response } from "express";
 import bcrypt, { hash } from "bcrypt-nodejs";
 import cors from "cors";
+import knex from "knex";
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '341256',
+        database: 'smart-brain'
+    }
+});
+
+// const knex = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host : '127.0.0.1',
+//       port : 3306,
+//       user : 'your_database_user',
+//       password : 'your_database_password',
+//       database : 'myapp_test'
+//     }
+// });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0, // used for tracking how many face uploaded
-            joined: new Date() // used for tracking when the accound first created
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0, // used for tracking how many face uploaded
-            joined: new Date() // used for tracking when the accound first created
-        }
-    ],
-    login: [
-        {
-            id: '123',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-
-app.get('/', (req, res) => {
-    res.send(database.users);
-});
-
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json("error logging in");
-    }
+    postgres.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return postgres.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
     const { email, password, name} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0, // used for tracking how many face uploaded
-        joined: new Date() // used for tracking when the accound first created
-    });
-    res.json(database.users[database.users.length - 1]);
+    const hash = bcrypt.hashSync(password);
+    postgres.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user);
+            })
+            .catch(err => res.status(400).json('unable to register'));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
 });
 
 app.get('/profile/:id', (req, res)=> {
     const { id } = req.params;
-    let found = false;
 
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    postgres.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not Found');
         }
-    });
-
-    if(!found) {
-        res.status(400).json('not found');
-    } 
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-
-    if(!found) {
-        res.status(400).json('not found');
-    } 
+    postgres('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries)
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
 });
 
 
